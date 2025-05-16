2e26865ae2a15361acfe683ddf14331a09b8e365
Henri Hosio <henajh@gmail.com>
2025-05-16 10:20:53 +0300

    get rid of non python code, move python code to src folder

--- ai/src/mastra/index.ts
+++
@@ -1,7 +0,0 @@
-import { Mastra } from "@mastra/core";
-
-import { intentAgent } from "./agents/intentAgent.js";
-
-export const mastra = new Mastra({
-  agents: { intentAgent },
-});
--- fe/src/views/index.ts
+++
@@ -1,4 +0,0 @@
-import TestView1 from "./TestView1";
-import TestView2 from "./TestView2";
-
-export { TestView1, TestView2 };
