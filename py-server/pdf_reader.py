3a3948ea8337e4046a65709eac2a9cca00c929fb
Henri Hosio <henajh@gmail.com>
2025-05-16 10:19:15 +0300

    use all files from docs dir

--- py-server/pdf_reader.py
+++ py-server/pdf_reader.py
@@ -1,7 +1,11 @@
+import os
+
 from llmsherpa.readers import LayoutPDFReader
 from llama_index.core import VectorStoreIndex, Document
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
+
+PATH = "./docs/"
 
 
 def read_pdf_file(query_text: str) -> str:
@@ -9,13 +13,22 @@ def read_pdf_file(query_text: str) -> str:
     embed = OllamaEmbedding(model_name="mxbai-embed-large")
 
     llmsherpa_api_url = "http://localhost:5010/api/parseDocument?renderFormat=all"
-    pdf_url = "./docs/company_car_policy.pdf"
     pdf_reader = LayoutPDFReader(llmsherpa_api_url)
-    doc = pdf_reader.read_pdf(pdf_url)
-
     index = VectorStoreIndex([], embed_model=embed)
-    for chunk in doc.chunks():
-        index.insert(Document(text=chunk.to_context_text(), extra_info={}))
+
+    obj = os.scandir(PATH)
+    files = []
+
+    for entry in obj:
+        if entry.is_file() and "pdf" in entry.name:
+            files.append(entry.name)
+
+    for f in files:
+        pdf_url = f"{PATH}{f}"
+        doc = pdf_reader.read_pdf(pdf_url)
+
+        for chunk in doc.chunks():
+            index.insert(Document(text=chunk.to_context_text(), extra_info={}))
 
     query_engine = index.as_query_engine(llm)
 
